ab60bcf47979f53ea6dcb9b200d53e15ba103ce6
jackydnb <jackydnb@gmail.com>
2022-11-02 18:32:36 +0300

    updated few things
    test now works

--- src/test/java/CucumberTest.java
+++ src/test/java/CucumberTest.java
@@ -3,11 +3,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        plugin = {"io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm", "pretty", "summary", "json:target/cucumber-report/report.json"},
-        glue = {"com/epam/jdi/http/stepdefs/en"}
-)
+@CucumberOptions(features = "src/test/resources/features/",
+        glue = {"com/epam/jdi/httptests/steps", "com/epam/jdi/http/stepdefs/en"},
+        tags = {"@smoke"})
 public class CucumberTest {
 
 }
--- src/test/java/com/epam/jdi/httptests/steps/InitService.java
+++ src/test/java/com/epam/jdi/httptests/steps/InitService.java
@@ -7,7 +7,7 @@ import static com.epam.http.requests.ServiceInit.init;
 import static com.epam.jdi.http.Utils.service;
 
 public class InitService {
-    @Given("init service example")
+    @Given("^I init service$")
     public void initService() {
         service.set(init(ServiceExample.class));
     }
