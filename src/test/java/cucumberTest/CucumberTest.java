9f602e8093b9eb738db20518405e130a7bbaabeb
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2023-01-06 23:24:44 -0500

    Update to latest version Cucmber (sync with 1.1.13 version)

---
+++ src/test/java/cucumberTest/CucumberTest.java
@@ -0,0 +1,14 @@
+package cucumberTest;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:features",
+        glue = {"cucumberTest/steps", "com/epam/jdi/http/stepdefs/en"},
+        plugin = {"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
+)
+public class CucumberTest {
+
+}
